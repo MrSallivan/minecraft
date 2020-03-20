2c575fde5a42af6a17924c7f459647d8cd4ad9f4
MrSallivan <nik.sallivan@gmail.com>
2020-03-20 12:17:14 +1000

    7

--- js/script.js
+++ js/script.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){	$('.slider').slick({			});	function ibg(){	$.each($('.ibg'), function(index, val) {		if($(this).find('img').length>0){			$(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');		}	});}ibg();});	
+$(document).ready(function(){	$('.news-item__body').slick({			});	function ibg(){	$.each($('.ibg'), function(index, val) {		if($(this).find('img').length>0){			$(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');		}	});}ibg();});	
